916d922f8b5266cbc285dbc74443a3fdb93e809a
Holger Hees <holger.hees@desy.de>
2025-07-27 18:05:40 +0200

    fix release for openhab 4

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,3 @@
-__version__ = "1.0.3" # version string is for backward compatibility with openhab 5.0.0
+__version__ = "1.0.4" # version string is for backward compatibility with openhab 5.0.0
 
 from openhab.helper import rule, logger, Registry, Timer
