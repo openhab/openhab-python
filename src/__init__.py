2100d8f9e1305ce5f5bbaffa8e879a799f4992d6
Holger Hees <holger.hees@desy.de>
2025-09-23 16:25:45 +0200

    prepare new release

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,3 @@
-__version__ = "1.0.7" # version string is for backward compatibility with openhab 5.0.0
+__version__ = "1.0.8" # version string is for backward compatibility with openhab 5.0.0
 
 from openhab.helper import rule, logger, Registry
